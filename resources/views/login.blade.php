69c20f92277f7d79b68bf50783fe761cb1559549
Moahmmad Rakib <mohammadrakib224@gmail.com>
2025-11-12 21:39:32 +0600

    Login page implemented and the necessary dependecies added for the project

---
+++ resources/views/login.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+        @vite('resources/css/app.css')
+
+</head>
+<body>
+    <div class="loginFormDiv">
+    <form method="" action="">
+<h2>Login</h2>
+<label name="emailFeild">Email:</label>
+<input type="email" id="emailF" name="email" placeholder="Enter Your Email ">
+<label name="roleFeild">Role:</label>
+<select name="employeeRole" id="employeeRole">
+<option value="admin">Admin</option>
+<option value="HR">HR Manager</option>
+<option value="employee">Employee</option>
+</select>
+<label name="passFeild">Password:</label>
+<input type="password" id="passwordF" name="password" placeholder="Enter Your Password">
+<button type="submit" class="submitBtn">Submit</button>
+    </form>
+    </div>
+</body>
+</html>

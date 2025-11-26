ca46289195ac587e079258b599d898860a0a11da
Moahmmad Rakib <mohammadrakib224@gmail.com>
2025-11-26 12:05:23 +0600

    blade file created

---
+++ resources/views/hr.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HR-Home</title>
+            @vite('resources/css/hr.css')
+
+</head>
+<body>
+    <h1 id="welcomeMsgA">Hello,HR</h1>
+    <div class="leftDiv">
+    <a href="" id="stat">Statistics</a>
+    <a href="" id="detailedInfo">Detailed Info</a>
+    <a href="" id="quickActions">Quick Actions</a>
+<button type="button" id="LogOutBtn" >Logout</button>
+    </div>
+    <div class="rightDiv">
+{{-- Here all the info show for the specific button of leftDiv --}}
+    </div>
+</body>
+</html>
